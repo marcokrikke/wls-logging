4a7ee6806e0afca06afd3000c00ba8d88df74f9c
Marco Krikke <marco@eveoh.nl>
2014-01-10 16:41:31 +0100

    Now using Logback and logging to file.
    - Use logback for logging instead of WLS logging
    - View log
    - Programmatically change log level
    - Configuration using JNDI and Deployment Plan

--- wls-logging-web/src/main/java/nl/eveoh/wls/loggingtest/IndexController.java
+++ wls-logging-web/src/main/java/nl/eveoh/wls/loggingtest/IndexController.java
@@ -1,13 +1,23 @@
 package nl.eveoh.wls.loggingtest;
 
+import ch.qos.logback.classic.Level;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class IndexController {
     private Logger log = LoggerFactory.getLogger(IndexController.class);
+
+    @Value("${logLocation}/wls-logging.log")
+    private String logLocation;
 
     @RequestMapping("/")
     public String index() {
@@ -18,5 +28,21 @@ public class IndexController {
         log.trace("This is a TRACE message.");
 
         return "index";
+    }
+
+    @RequestMapping(value = "/log", produces="text/plain")
+    @ResponseBody
+    public String viewLog() throws IOException {
+        return FileUtils.readFileToString(new File(logLocation));
+    }
+
+    // http://localhost:8080/logLevel?level=WARN
+    @RequestMapping(value = "/logLevel", params = "level", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void changeLogLevel(@RequestParam("level") Level newLogLevel) {
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(newLogLevel);
+
+        log.info("Log level set to: " + root.getLevel().toString());
     }
 }
